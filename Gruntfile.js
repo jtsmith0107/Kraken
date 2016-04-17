431b3b8baf2ef1bf1a08112d11e3b3e3348a2966
Andre Tran <andre.tran@rallyhealth.com>
2016-04-16 17:27:12 -0700

    Setup grunt to watch and compile typescript files

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-typescript');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+//    grunt.loadNpmTasks('grunt-open');
+ 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: './'
+                }
+            }
+        },
+        typescript: {
+            base: {
+                src: ['web/src/**/*.ts'],
+                dest: 'web/dist/',
+                options: {
+                    module: 'amd',
+                    target: 'es5',
+                    sourceMap: true
+                }
+            }
+        },
+        watch: {
+            files: '**/*.ts',
+            tasks: ['typescript']
+        }
+    });
+ 
+    grunt.registerTask('default', ['connect', 'watch']);
+}
